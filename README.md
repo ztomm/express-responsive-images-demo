edaaf9b9766d112018a38e968ebe96785ffa4cb5
Murat Karaca <dev@muratkaraca.de>
2019-11-03 13:49:54 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# express-responsive-images DEMO
+
+A minimal application to demonstrate the express-responsive-images module.  
+
+## install
+
+```bash
+npm i express-responsive-images-demo
+```
+
+## start
+
+```bash
+npm run start
+```
+
+Open [http://localhost:3000](http://localhost:3000) in browser.  
+
+In the `public` directory you will find a newly created folder: `images-cache`. Resize the browser window, reload the page and see the `public` directory what happened.  
+
+## options can be found here:
+
+[express-responsive-images on github.com](https://github.com/xkmgt/express-responsive-images)  
+[express-responsive-images on npmjs.org](https://www.npmjs.com/package/express-responsive-images)  
+  
---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan')
+
+const app = express();
+app.set('host', 'localhost');
+app.set('port', 3000);
+
+app.use(logger('tiny'))
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// route to index page 
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '/views/index.htm'));
+});
+
+// require module
+const responsiveImages = require('express-responsive-images');
+
+// add module as middleware to express
+app.use(
+    responsiveImages({
+        staticDir: '/public',
+        watchDirectories: ['/images'],
+        directScaling: true,
+        directScaleSizes: [200, 400],
+        debug: true,
+    })
+);
+
+// static route
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+// start express server
+app.listen(app.get('port'), () => {
+    console.log(`App is running at ${app.get('host')}:${app.get('port')}`);
+    console.log(`Press CTRL-C to stop\n`);
+});
